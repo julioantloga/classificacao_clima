90b90ccfb421ce29f4b4059bcf3328044a688fa0
Julio Antloga <julio.antloga@mindsight.com.br>
2025-08-04 14:36:48 -0300

    database new config

--- db_config.py
+++ db_config.py
@@ -1,5 +1,5 @@
 import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = os.environ.get("ca8lne8pi75f88.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com")
+DATABASE_URL = os.environ.get("postgres://u7v0qvouq99ed7:pac5c0429a529fce61761c3776da333dd681668f2bde519e87027063fb0c6d880@ca8lne8pi75f88.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d2i1sotad13i9")
 engine = create_engine(DATABASE_URL)
