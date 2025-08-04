bd06b8d7cd2f8ada423dc2b0c017b476f8435a44
Julio Antloga <julio.antloga@mindsight.com.br>
2025-08-04 13:14:23 -0300

    edit configuration 1

--- db_config.py
+++ db_config.py
@@ -1,5 +1,5 @@
 import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("ca8lne8pi75f88.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com")
 engine = create_engine(DATABASE_URL)
