182771a9121949c3a7fe3c00e98266522bc10f65
Julio Antloga <julio.antloga@mindsight.com.br>
2025-10-01 14:04:36 -0300

    v2.2 banco

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 #from dotenv import load_dotenv
 #load_dotenv()
 
-DATABASE_URL = os.environ.get("DATABASE_URL") #"postgresql://postgres:eFOZsAcZcJJcruKjzWFUZieWfhZMqDvV@shortline.proxy.rlwy.net:55123/railway"
+DATABASE_URL = "postgresql://postgres:eFOZsAcZcJJcruKjzWFUZieWfhZMqDvV@shortline.proxy.rlwy.net:55123/railway" #os.environ.get("DATABASE_URL")
 BASE_URL = os.environ.get("BASE_URL")
 
 # Corrige o prefixo do Heroku se necessário
